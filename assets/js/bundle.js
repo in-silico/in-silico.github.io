8fd716e505ec51953cfef797001caa59126ad857
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-08-24 10:52:11 -0500

    maxHeight in cards

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -7,6 +7,8 @@
 
 (function() {
   var $ = require('jquery');
+  changeHeight('.tabla-contenido');
+
   $('.group').click(function() {
     // alert("hola");
     $('.active').removeClass('active');
@@ -19,7 +21,7 @@
     $(id).addClass('active');
 
     if (id == "#nav-inicio") {
-      $(".navbar").css('display', 'none');
+      $(".navbar").css( 'display', 'none');
     }
     else {
       $(".navbar").css('display', 'initial');
@@ -37,6 +39,20 @@
       }, 1000);
     }
   });
+
+  function changeHeight(element) {
+    let $el = $(element);
+    let maxHeight = 0;
+
+    for (let i = 0; i < $el.length; i++) {
+      let height = Number($($el[i]).css('height').split('px')[0]);
+      maxHeight = Math.max(maxHeight, height);
+    }
+
+    for (let i = 0; i < $el.length; i++) {
+      $($el[i]).css('height', `${maxHeight}px`);
+    }
+  }
 })();
 
 },{"jquery":2}],2:[function(require,module,exports){
--- build/main.js
+++ build/main.js
@@ -6,6 +6,8 @@
 
 (function() {
   var $ = require('jquery');
+  changeHeight('.tabla-contenido');
+
   $('.group').click(function() {
     // alert("hola");
     $('.active').removeClass('active');
@@ -36,4 +38,18 @@
       }, 1000);
     }
   });
+
+  function changeHeight(element) {
+    let $el = $(element);
+    let maxHeight = 0;
+
+    for (let i = 0; i < $el.length; i++) {
+      let height = Number($($el[i]).css('height').split('px')[0]);
+      maxHeight = Math.max(maxHeight, height);
+    }
+
+    for (let i = 0; i < $el.length; i++) {
+      $($el[i]).css('height', `${maxHeight}px`);
+    }
+  }
 })();
